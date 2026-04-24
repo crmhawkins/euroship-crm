29615c0bb53f369af23859080ed96a49a0cc236e
HawkinsWeb <128600888+crmhawkins@users.noreply.github.com>
2026-04-24 14:58:03 +0200

    feat: beautiful split-screen login page with Euroship branding

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -27,7 +27,7 @@ class AdminPanelProvider extends PanelProvider
             ->default()
             ->id('admin')
             ->path('admin')
-            ->login()
+            ->login(\App\Filament\Pages\Auth\Login::class)
             ->brandName('Euroship CRM')
             ->brandLogo('https://euroshipspain.com/wp-content/uploads/2023/01/logo.svg')
             ->favicon('https://euroshipspain.com/wp-content/uploads/2023/01/logo.svg')
